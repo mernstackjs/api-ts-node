0fc1182a5a66f68f6ee7d7aeb28f2aa8a06a33db
mernstackjs <mernstack.js@gmail.com>
2024-07-20 00:01:25 +0200

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,36 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+// Middleware
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({ message: err.message });
+});
+app.get("/", (req, res) => {
+    const users = [
+        {
+            id: 1,
+            name: "Ahmed",
+        },
+        {
+            id: 2,
+            name: "Muna",
+        },
+    ];
+    res.status(200).json({
+        success: false,
+        message: "is working",
+        users,
+    });
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+
+const app: Application = express();
+type usersProp = {
+  id: number;
+  name: string;
+};
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Error handling middleware
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+  res.status(500).json({ message: err.message });
+});
+
+app.get("/", (req: Request, res: Response) => {
+  const users: usersProp[] = [
+    {
+      id: 1,
+      name: "Ahmed",
+    },
+    {
+      id: 2,
+      name: "Muna",
+    },
+  ];
+  res.status(200).json({
+    success: false,
+    message: "is working",
+    users,
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
